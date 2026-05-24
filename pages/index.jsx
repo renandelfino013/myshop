631977c2f65ac232e090cbc3294b3b369014486c
renandelfino013 <renancontaps@gmail.com>
2026-05-24 11:20:49 -0300

    start

---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import css from "../styles/Home.module.css";
+
+function Home() {
+  return (
+    <div className={css.container}>
+      <h1>Welcome to My Shop</h1>
+      <p>This is a simple e-commerce site.</p>
+    </div>
+  );
+}
+export default Home;

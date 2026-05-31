5525724afc7b077dd3c11894eb11df903f4af032
renandelfino013 <renancontaps@gmail.com>
2026-05-31 17:07:37 -0300

    try

--- utils/db.js
+++ utils/db.js
@@ -5,8 +5,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: parseInt(process.env.DB_PORT, 10) || 5432,
 });
 
 export default pool;
-  

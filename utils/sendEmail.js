4c11fa577bc0eb6eb1ffb2147bd178c5805df9eb
renandelfino013 <renancontaps@gmail.com>
2026-06-03 16:15:27 -0300

    try fix 2

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,12 +1,11 @@
 import nodemailer from "nodemailer";
 async function sendLoginNotification(to, subject, html) {
-  let transporter = nodemailer.createTransport({
+  const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
-    service: "gmail",
     auth: {
-      user: process.env.EMAIL_USE,
+      user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
